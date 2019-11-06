44b48bc7b02244565cd20e29df17ca443b2e79ba
Alan Luo <i@iltc.io>
2019-11-06 11:51:30 -0500

    Initial Commit

---
+++ GoogleSignInDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoogleSignInDemo
+//
+//  Created by Alan Luo on 11/6/19.
+//  Copyright © 2019 iLtc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
